0b9ba4bb71b136c61a0a91dd163c663173ddece0
kvoyant <kvoyant@gmail.com>
2019-12-15 10:51:16 +0900

    메인에서 뒤로가기시 종료 메세지 보이게 처리

--- app/src/main/java/com/yhkim/kt_20191215_liftcycle/MainActivity.kt
+++ app/src/main/java/com/yhkim/kt_20191215_liftcycle/MainActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    var lastBackButtonTime = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +18,12 @@ class MainActivity : BaseActivity() {
         setValues()
 
         Log.d("생명주기", "onCreate 실행")
+    }
+
+    override fun onBackPressed() {
+        val currentTime = System.currentTimeMillis()
+
+        Toast.makeText(mContext, "한번 더 뒤로 버튼을 누르면 종료됩나다", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
