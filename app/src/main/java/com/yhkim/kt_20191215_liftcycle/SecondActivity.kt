1f79f9d72e15bb305b2721489699130187128337
kvoyant <kvoyant@gmail.com>
2019-12-15 10:48:32 +0900

    백버튼 이벤트 커스터마이징

--- app/src/main/java/com/yhkim/kt_20191215_liftcycle/SecondActivity.kt
+++ app/src/main/java/com/yhkim/kt_20191215_liftcycle/SecondActivity.kt
@@ -1,7 +1,9 @@
 package com.yhkim.kt_20191215_liftcycle
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class SecondActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,20 @@ class SecondActivity : BaseActivity() {
         setContentView(R.layout.activity_second)
         setupEvents()
         setValues()
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed() //주석하면 뒤로가기 안먹힘
+
+        val alert = AlertDialog.Builder(mContext)
+        alert.setTitle("게시글 작성 중지 확인")
+        alert.setTitle("정말 글 작성을 중단 하시겠습니까? 작성하신 내용은 모두 삭제 됩니다.")
+        alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+            finish()
+        })
+        alert.setNegativeButton("취소", null)
+        alert.show()
+
     }
     override fun setupEvents() {
     }
